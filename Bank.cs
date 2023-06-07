4f820b64b0060c61bc99dba374309a92720dea51
Alex Bishop <alexb98@gmail.com>
2023-06-07 09:21:10 -0500

    Initial Commit, first bullet completed

---
+++ Bank.cs
@@ -0,0 +1,17 @@
+namespace Group_HeistPt2
+{
+    public class Bank
+    {
+        public int CashOnHand { get; set; }
+        public int AlarmScore { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
+        public bool IsSecure
+        {
+            get
+            {
+                return AlarmScore > 0 || VaultScore > 0 || SecurityGuardScore > 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Group_HeistPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
